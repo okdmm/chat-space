72057c4d46f5ad857e3e78abd9592a25724070d8
okada <kirinnremonn1122@gmail.com>
2018-04-13 13:26:26 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:edit, :update, :new, :create] do
+    resurces :messages, only[:index, :create]
+  end
 end
