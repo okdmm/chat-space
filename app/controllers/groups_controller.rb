af7c48697211e68ec8496272ae7c29af4ddbee6a
okada <kirinnremonn1122@gmail.com>
2018-04-14 11:15:49 +0900

    アクションの作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,18 @@ class GroupsController < ApplicationController
   end
   
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループが作成されました。"
+    else
+      render: nwe
+    end
   end
 
   def update
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name, {user_ids: []})
 end
