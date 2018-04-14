97f79263d07b7018541e4fccb591fdb1072faa05
okada <kirinnremonn1122@gmail.com>
2018-04-14 15:07:55 +0900

    ルートにメッセージ送信のためのcreateを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:edit, :update, :new, :create] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
 end
