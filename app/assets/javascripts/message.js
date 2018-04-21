cff7efddf0ddb2da0c6bd01012e20e442cd8cbd6
okada <kirinnremonn1122@gmail.com>
2018-04-22 06:32:41 +0900

    ajaxで返すhtmlを指定

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,46 @@
 $(function(){
+  function buildHTML(message){
+    var img = '';
+    if (message.image){
+    img = '<img src="${message.image}">'
+    }
+    var html = `<div class="chat-main__message">
+                  <div class="chat-main__message-name">
+                    ${ message.name } 
+                  </div>
+                  <div class="chat-main__message-time">
+                    ${ message.created_at }
+                  </div>
+                  <div class="chat-main__message-body">
+                    ${ message.content }
+                  <img class='message_image'>
+                    ${img}
+                  </img>
+                  </div>
+                </div>`
+    return html; 
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-
     $.ajax({
       type: 'POST',
       url: url,
       data: formData,
-      dataType: 'json'
+      dataType: 'json',
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__body--messages-list').append(html)
+      $('#message_content').val('')
+    })
+    .fail(function(){
+      alert('メッセージを入力してください');
+    })
+    return false;
   })
 })
