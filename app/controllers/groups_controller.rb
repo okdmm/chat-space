767a106fabb221f22c98735baed315ecfae50d00
okada <kirinnremonn1122@gmail.com>
2018-04-30 23:35:04 +0900

    不要な部分を削除　申し訳程度にeditを追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,11 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @group.users << current_user
   end
   
+  def edit
+  end
+
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -20,7 +22,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループを編集しました。'
+      redirect_to root_path,  notice: 'グループを編集しました。'
     else
       render :edit
     end
